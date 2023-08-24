8e98b31f23b7cd5a5a593de94174bbfe23b637a0
Saad <rs.mailpro@gmail.com>
2023-08-24 13:39:28 +0200

    Formulaire de connexion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Container from './component/Container'
 import HomePage from './component/HomePage'
@@ -9,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 export default function Login() {
+
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
   return (
-    <div>Login</div>
+    <div className="container">
+        <h1>Connexion</h1>
+        <form onSubmit={(e)=>{
+            e.preventDefault()
+            if(email === 'contact@cloudcampus.fr' && password === '0000'){
+                return navigate('/')
+            }
+            alert('Identifiants ou mot de passe incorrect')
+        }}>
+            <div className='mb-3 form-group'>
+                <label>Login</label>
+                <input type="text" className="form-control" value={email} onChange={(e)=>{ 
+                    setEmail(e.target.value) 
+                }} />
+            </div>
+            <div className="form-group">
+                <label>Mot de passe</label>
+                <input type="password" className="form-control"  value={password} onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}/>
+            </div>
+            <input className='btn btn-primary ' type="submit" value="Connexion" />
+        </form>
+
+    </div>
   )
 }
